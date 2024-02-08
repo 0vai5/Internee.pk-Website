267a2a78955d85c69c0ab0e956e7056880cf0348
Ovais Raza <rovais53@gmail.com>
2024-02-08 17:54:15 +0500

    Implement Locomotive Scroll for smooth scrolling.

--- TSK-000-36/script.js
+++ TSK-000-36/script.js
@@ -1,12 +1,5 @@
-let navlinks = document.getElementById('navlinks');
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('body'),
+    smooth: true
+});
 
-function openmenu() {
-    navlinks.style.display = "block"
-    navlinks.style.right = "0";
-    
-}
-
-function hidemenu() {
-    navlinks.style.right = "-200px";
-    navlinks.style.display = "none";
-}
