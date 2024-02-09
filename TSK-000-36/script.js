391dea1a180c3271841203f75f96dd55ec50ce52
Ovais Raza <rovais53@gmail.com>
2024-02-09 12:04:27 +0500

    added form validation and completed contact page

--- TSK-000-36/script.js
+++ TSK-000-36/script.js
@@ -3,3 +3,22 @@ const scroll = new LocomotiveScroll({
     smooth: true
 });
 
+function validateForm() {
+    var email = document.getElementById('email').value;
+    var message = document.getElementById('message').value;
+
+    // Email validation
+    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+        document.getElementById('emailError').innerText = 'Invalid email address';
+        return false;
+    } else {
+        document.getElementById('emailError').innerText = '';
+    }
+
+    // Additional validation for other fields can be added here
+
+    // If all validations pass, the form will be submitted
+    return true;
+}
+
